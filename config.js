ccf3d959016d95fbf677c44a8a94214d6c91ef72
sanjeev <sanjeevsanjeevbaghel@gmail.com>
2020-08-11 21:32:48 +0530

    custom setter

--- config.js
+++ config.js
@@ -11,3 +11,30 @@ Ext.define('Person',{
 var obj = new Person({name: 'SANJEEV', gender: 'MALE'});
 alert(obj.getName());
 alert(obj.getGender());
+obj.setName('Name changed');
+obj.setGender('Gender changed');
+alert("changed " + obj.getName());
+alert("changed " + obj.getGender());
+
+//custom setter
+
+Ext.define('Employee',{
+    config: {
+        name: 'unamed',
+        gender: 'unknown',
+        salary: 0
+    },
+    applySalary: function(sal){
+        return sal * 10;
+    },
+    updateSalary: function(oldVal, newVal){
+        return 'old salary = '+ oldVal + ' new salary = '+ newVal;
+    },
+    constructor: function(config){
+        this.initConfig(config);
+    }
+});
+obj = new Employee({name: 'Sanjeev',gender: 'Male', salary: 10000});
+alert(obj.getName());
+alert(obj.getGender());
+alert(obj.getSalary());
